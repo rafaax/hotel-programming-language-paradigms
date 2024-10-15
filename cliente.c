21aed1eca6394512d349a8aa2dcfaf19caf25d81
raphael <rafavfx1@gmail.com>
2024-10-15 20:17:50 -0300

    r

--- cliente.c
+++ cliente.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
+#include <time.h>
+
+
+int id_cliente;
+char nome_cliente[];
+char cpf_cliente[];
+int ativo;
+char data_nascimento_cliente[];
+char criado_em_cliente[];
+
+
+void criaCliente(){
+	
+}
+
+void editaCliente(){
+	
+}
+
+void deletaCliente(){
+	
+}
+
+void listarClientes(){
+	
+}
+
+void verCliente(){
+	
+}
 
--- cliente.h
+++ cliente.h
@@ -1,1 +1,9 @@
+void criaCliente()
 
+void editaCliente()
+
+void deletaCliente()
+
+void listarClientes()
+
+void verCliente()
