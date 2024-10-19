48ac91314aa3fa50f2ccabf4c8a2370431fdd617
raphael <rafavfx1@gmail.com>
2024-10-19 17:56:55 -0300

    cliente.h

--- cliente.h
+++ cliente.h
@@ -1,1 +1,21 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
 
+extern int id_cliente;
+extern char nome_cliente[];
+extern char cpf_cliente[];
+extern int ativo;
+extern char data_nascimento_cliente[];
+extern char criado_em_cliente[];
+
+void criaCliente();
+
+void editaCliente();
+
+void deletaCliente();
+
+void listarClientes();
+
+void verCliente();
+
+#endif
