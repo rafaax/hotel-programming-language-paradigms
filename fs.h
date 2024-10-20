f581d969b4c7e1bcf20934c19bc2d49a5d21368c
raphael <rafavfx1@gmail.com>
2024-10-19 22:28:33 -0300

    a

--- fs.h
+++ fs.h
@@ -1,4 +1,11 @@
 #ifndef FS_H
 #define FS_H
 
+extern cJSON *json = cJSON_CreateObject();
+
+void cliente_to_json();
+void quarto_to_json();
+void reserva_to_json();
+void servico_to_json();
+
 #endif
